313bc3f09b87269fd8d3f4b2200b726bbd700477
17010457 <17010457@myrp.edu.sg>
2018-08-08 12:10:43 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/dmsd/mywebbrowser/MainActivity.java
@@ -0,0 +1,28 @@
+package sg.edu.rp.dmsd.mywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String url = "https://www.rp.edu.sg";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
